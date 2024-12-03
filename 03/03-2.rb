58d3cf8b4e22bb71e80454c9c10869896923517a
Quinn Daley <quinn@fishpercolator.co.uk>
2024-12-03 07:48:21 +0000

    A nicer implementation without hideous global variables

--- 03/03-2.rb
+++ 03/03-2.rb
@@ -1,18 +1,3 @@
 #!/usr/bin/env ruby
 
-# Hideous global variables
-on = true
-sum = 0
-
-ARGF.read.scan %r{(do|don't|mul)\(((\d+),(\d+))?\)} do |inst, _, x,y|
-  case inst
-  when 'do'
-    on = true
-  when "don't"
-    on = false
-  when 'mul'
-    sum += x.to_i*y.to_i if on
-  end
-end
-
-p sum
+p ARGF.read.scan(%r{(do|don't|mul)\(((\d+),(\d+))?\)}).map {|i,_,x,y| i=="don't"..i=="do" ? 0 : x.to_i*y.to_i }.sum
