d7463780c7886bb261fb38d1453d7fac09afeec8
Quinn Daley <quinn@fishpercolator.co.uk>
2024-12-06 08:22:53 +0000

    Many optimizations available but my laptop is powerful enough to just do it this way

--- 06/06.rb
+++ 06/06.rb
@@ -1,20 +1,25 @@
 #!/usr/bin/env ruby
 
+require 'set'
 require_relative '../utils/grid'
 
 class Lab < Grid
-  attr_accessor :dir
+  class LoopError < StandardError; end
+
+  attr_accessor :dir, :have_been
 
   def initialize(io)
     super
     find_guard!
-    self.dir = :north
+    self.have_been = Set.new
+    @reset = cells.dup
   end
 
   def color(v, c)
     case c
     when '#' then {color: :yellow}
     when 'X' then {color: :red}
+    when 'O' then {color: :green, mode: :bold}
     when '^' then {color: :white, mode: :bold}
     else {color: :grey}
     end
@@ -25,9 +30,14 @@ class Lab < Grid
   end
 
   def move_guard!
-    if ['.', 'X', nil].include? self[move(dir)]
+    next_v = move(dir)
+    if ['.', 'X', nil].include? self[next_v]
       self[cursor] = 'X'
       move!(dir)
+      # If the guard has been to this exact same spot facing the same direction,
+      # we're in a loop
+      raise LoopError if have_been.include? [cursor, dir]
+      have_been << [cursor, dir]
     else
       self.dir = next_dir
     end
@@ -42,15 +52,43 @@ class Lab < Grid
     count {|_,c| c == 'X'}
   end
 
+  def reset!
+    self.cells = @reset.dup
+    self.have_been = Set.new
+    find_guard!
+  end
+
+  # Try placing an obstacle at every location and count the ones that result
+  # in a LoopError
+  def n_loop_obstacles
+    loops = 0
+    each do |v,c|
+      reset!
+      next if self[v] != '.'
+      self[v] = 'O'
+      begin
+        run!
+      rescue LoopError
+        puts self
+        loops += 1
+      end
+    end
+    loops
+  end
+
   private
 
   def find_guard!
-    find {|v,c| c == '^'}.then {|v,_| self.cursor = v}
+    find {|v,c| c == '^'}.then {|v,_| self.cursor = v; self.dir = :north }
   end
 
 end
 
+# Part 1
 l = Lab.new(ARGF)
 l.run!
 puts l
 p l.count_visited
+
+# Part 2 - this takes a long time to run and prints a lot of output
+p l.n_loop_obstacles
