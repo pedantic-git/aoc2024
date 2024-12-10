ae7e002a3b44703b88825d20052d5f35d5cb384c
Quinn Daley <quinn@fishpercolator.co.uk>
2024-12-10 08:15:38 +0000

    Except... I stumbled across the solution to part 2 while I was writing part 1

---
+++ 10/10-2.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+require_relative '../utils/grid'
+
+class Island < Grid
+
+  def load_cell(c,y,x)
+    self[y,x] = c  == '.' ? nil : c.to_i
+  end
+
+  def render(c,y,x)
+    c ? c.to_s : '.'
+  end
+
+  # Find all the locations of the trailheads on the map
+  def trailheads
+    select {|v,c| c == 0}.keys
+  end
+
+  # Given a trailhead (or any cell) count how many 9s are reachable from it
+  def score(v=cursor, seen=[])
+    return 1 if self[v] == 9 # base case - we've found one
+    valid_moves = neighbours_with_char(self[v] + 1, v)
+    return 0 if valid_moves.empty? # dead end
+    return valid_moves.sum {score(_1)}
+  end
+
+  # Get the score for all the trailheads and add them together
+  def total_score
+    trailheads.sum {score(_1)}
+  end
+
+end
+
+i = Island.new(ARGF)
+p i.total_score
