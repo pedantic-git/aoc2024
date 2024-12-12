21f587e0c5a19c5b7d72404cade3ad245fc0378c
Quinn Daley <quinn@fishpercolator.co.uk>
2024-12-12 08:06:44 +0000

    Solution to part1 - it's slow but it works

--- 12/12-1.rb
+++ 12/12-1.rb
@@ -5,22 +5,23 @@ require_relative '../utils/grid'
 class Farm < Grid
 
   attr_reader :regions
+  attr_accessor :n_regions
 
   def initialize(io)
     super
     @regions = {}
+    @n_regions = 0
     detect_regions!
   end
 
   def detect_regions!
-    current_region = 1
     each do |v,c|
       existing = neighbours_with_char(c, v).select { regions.key? _1 }
       existing_regions = regions.values_at(*existing).sort.uniq
       if existing.length == 0
         # We've found a new region
-        regions[v] = current_region
-        current_region += 1
+        self.n_regions += 1
+        regions[v] = n_regions
       elsif existing_regions.length == 1
         # We're just adding to a region
         regions[v] = regions[existing.first]
@@ -28,9 +29,19 @@ class Farm < Grid
         # Merge two or more regions
         keep, *go = existing_regions
         regions[v] = keep
+        # This could be faster but it's fast enough
         regions.each {|v,c| regions[v] = keep if go.include? regions[v]}
       end
     end      
+  end
+
+  def price(region)
+    squares = Set.new(regions.keys.select {regions[_1] == region})
+    squares.length * squares.sum {|x| directions.count {|_,m| !squares.include? x+m} }
+  end
+
+  def total_price
+    1.upto(n_regions).sum { price _1 }
   end
 
   REGION_COLORS = {0 => :red, 1 => :cyan, 2 => :magenta, 3 => :green, 4 => :blue, 5 => :yellow, 6 => :light_red, 7 => :light_cyan, 8 => :light_green, 9 => :light_blue}
@@ -42,5 +53,5 @@ class Farm < Grid
 end
 
 f = Farm.new(ARGF)
-p f.regions
 puts f
+p f.total_price
