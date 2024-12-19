99c3b50f15b56a17d783d48ef7d7770d56d9b0c5
Quinn Daley <quinn@fishpercolator.co.uk>
2024-12-19 07:52:33 +0000

    The one time memoization is exactly the answer!

--- 19/19-2.rb
+++ 19/19-2.rb
@@ -1,14 +1,17 @@
 #!/usr/bin/env ruby
 
-@towels = ARGF.readline.chomp.split(', ').sort_by(&:length).reverse
+@towels = ARGF.readline.chomp.split(', ').sort_by(&:length)
 re = Regexp.new("\\A(#{@towels.join('|')})+\\Z".freeze)
 
+@memo = {}
+
 def n_combinations(pattern)
+  return @memo[pattern] if @memo.key?(pattern)
   # Find all the matching prefixes
   prefixes = @towels.select {pattern.start_with? _1}
   # Count the prefixes that end the string and recurse with the rest
   ends, conts = prefixes.partition { _1.length == pattern.length }
-  ends.length + conts.sum { n_combinations(pattern[_1.length..-1]) }
+  @memo[pattern] = ends.length + conts.sum { n_combinations(pattern[_1.length..-1]) }
 end
 
 p ARGF.select {re.match? _1}.sum {|pattern| n_combinations(pattern.chomp) }
